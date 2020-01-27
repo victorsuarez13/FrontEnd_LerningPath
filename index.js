a729a5e839d745ffd75c2616f3cd3b9ee082e438
victorsuarez13 <victor.suarez.cardenas@gmail.com>
2020-01-27 10:40:46 -0500

    specific index to initialize the server

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const {mongoose}= require('./controller/connection')
+
 
 app.set('port', 3000);
 
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.use('/app/',require('./routes/route'));
 app.use(express.static('./public/'))
 
 app.listen(app.get('port'), () => {
